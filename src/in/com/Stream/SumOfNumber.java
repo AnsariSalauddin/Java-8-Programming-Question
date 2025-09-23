34d6cf172f9432250ee22d2881c5515b43be84b7
Salauddin Ansari <salauddinansari07457@gmail.com>
2025-09-23 23:12:57 +0530

    add Method Reference

--- src/in/com/Stream/SumOfNumber.java
+++ src/in/com/Stream/SumOfNumber.java
@@ -8,8 +8,14 @@ public class SumOfNumber {
 
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 3, 4, 4, 5);
+        //use Lambda expression (a,b)->a+b
         Optional<Integer> reduce = list.stream().reduce((a, b) -> a + b);
         Integer integer = reduce.get();
         System.out.println("sum of Number:"+integer);
+
+        Optional<Integer> reduce1 = list.stream().reduce(Integer::sum);
+        Integer integer1 = reduce1.get();
+        System.out.println(integer1);
+
     }
 }
