d24420f0f14850b07a375071eb504260f1af9fc7
Salauddin Ansari <salauddinansari07457@gmail.com>
2025-09-23 23:08:23 +0530

    sum of Number using stream

---
+++ src/in/com/Stream/SumOfNumber.java
@@ -0,0 +1,15 @@
+package in.com.Stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class SumOfNumber {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 3, 4, 4, 5);
+        Optional<Integer> reduce = list.stream().reduce((a, b) -> a + b);
+        Integer integer = reduce.get();
+        System.out.println("sum of Number:"+integer);
+    }
+}
